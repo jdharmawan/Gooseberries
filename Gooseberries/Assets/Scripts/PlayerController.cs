c41cbb6d8d9a87e0e28252777cc87f49ae0cc457
zeeloon <keybladeryss@gmail.com>
2022-06-25 17:43:52 +0800

    Player Controls
    Added left right movement and jump functionality.
    
    Added states to control player functions and to help with future animation controls.
    
    Added ground collider as a child object.

--- Gooseberries/Assets/Scripts/PlayerController.cs
+++ Gooseberries/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//think about how weighty we want the game to feel
+//fast paced or slower
+//eg if we want to have a anim when recovering from fall, might need to have another state
+//wall jump? i just thought of the code, can just set isgrounded to true when touching walls or smth, of course the physics is more complex
+
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D groundedCol;
+
     private BoxCollider2D col;
     private Rigidbody2D rb2d;
 
     //might need states to keep track of like shooting arrow, deploying shield etc
-    private enum playerState {Idle, Walking, Charging};
-    private bool isGrounded;
+    private enum playerState {Idle, Walking, Aiming, Shooting, Reloading};
+    //gonna need to set up a seperate smaller collider below the player collider to keep track of grounded
+    [SerializeField] private bool isGrounded;
 
     private playerState pState;
     private Vector2 force;
 
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
+    private int hp = 3;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float jumpForce = 2f;
     [SerializeField] private float bowCharge;//temp, not sure if gonna use in the end
 
 
@@ -33,25 +42,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         GetPlayerInput();
-        MoveCharacter();
 
-        //slowly reset speed to 0
-        if(moveSpeed > 0)
-        {
-            moveSpeed -= 1f;
-        }
-        else if(moveSpeed < 0)
-        {
-            moveSpeed += 1f;
-        }
+        //test quick deceleration
+        if (moveSpeed != 0)
+            moveSpeed = Mathf.Lerp(moveSpeed, 0, 1f);
 
-        //this one not so simple
-        //if want to decelerate, must wait till max jump height
-        //if not we just use gravity
-        if(jumpForce > 0)
-        {
-            jumpForce -= 1f;
-        }
     }
 
     private void GetPlayerInput()
@@ -64,24 +59,84 @@ public class PlayerController : MonoBehaviour
         //{
         //}
 
-        if (Input.GetKey(KeyCode.A))
+        if(Input.GetMouseButtonDown(0))
         {
-            moveSpeed = -2f;
+            //if left click
+            pState = playerState.Aiming;
         }
-        else if (Input.GetKey(KeyCode.D))
+
+        if(Input.GetMouseButtonUp(0))
         {
-            moveSpeed = 2f;
+            //when release mouse, shoot arrow and maybe idle, or reload
+            pState = playerState.Shooting;
+            Shoot();
         }
-        else if (Input.GetKeyDown(KeyCode.Space))
+
+        if(Input.GetMouseButtonDown(1))
         {
-            jumpForce = 2f;
+            //if right click do shield
+
         }
+
+        if(Input.GetMouseButtonUp(1))
+        {
+            //release shield
+        }
+
+        if (pState != playerState.Aiming)
+        {
+            //need to consider if we want like jump A D movement
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (isGrounded)
+                    pState = playerState.Walking;
+
+                //move left
+                moveSpeed = -2f;
+                rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                if (isGrounded)
+                    pState = playerState.Walking;
+
+                //move right
+                moveSpeed = 2f;
+                rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (isGrounded)
+                {
+                    jumpForce = 5f;
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+                    isGrounded = false;
+                    //pState = playerState.Jumping;
+                }
+            }
+        }
+
+        //Debug.Log("player state: " + pState.ToString());
     }
 
     private void MoveCharacter()
     {
-        force = new Vector2(moveSpeed, jumpForce);
+        force = new Vector2(moveSpeed, 0);
         rb2d.AddForce(force);
+    }
+
+    //below 2 functions might be enumerator, coz need to deal with eventual animations
+    void Shoot()
+    {
+        //do shooting, handle anims then set to reloading
+        pState = playerState.Reloading;
+        Reload();
+    }
+
+    void Reload()
+    {
+        //reload then go back to idle
+        pState = playerState.Idle;
     }
 
     //for when anims are done
@@ -97,8 +152,14 @@ public class PlayerController : MonoBehaviour
                 case playerState.Walking:
                     Debug.Log("Walking");
                     break;
-                case playerState.Charging:
-                    Debug.Log("Charging");
+                case playerState.Aiming:
+                    Debug.Log("Aiming");
+                    break;
+                case playerState.Shooting:
+                    Debug.Log("Shooting");
+                    break;
+                case playerState.Reloading:
+                    Debug.Log("Reloading");
                     break;
                 default:
                     break;
@@ -108,5 +169,10 @@ public class PlayerController : MonoBehaviour
         {
             //do jump anim
         }
+    }
+
+    public void SetIsGrounded(bool b)
+    {
+        isGrounded = b;
     }
 }
