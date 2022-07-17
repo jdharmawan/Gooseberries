d1f5ca44ec73dc5242177137f669e01b5007eaa8
shaunwzl <keybladeryss@gmail.com>
2022-07-17 09:14:52 +0800

    Audio Manager and some sound files

---
+++ Gooseberries/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+
+        Play("Possible Credits BGM");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.Log("sound not found");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void StopAllSounds()
+    {
+
+    }
+}
