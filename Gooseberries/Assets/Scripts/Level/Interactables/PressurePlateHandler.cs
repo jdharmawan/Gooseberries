5dece0c54d035cef50f631ac7cd5f4a6295c0e1c
jdharmawan <d.harmawan.joshua@gmail.com>
2022-07-17 17:03:25 +0800

    new tiles amd level

--- Gooseberries/Assets/Scripts/Level/Interactables/PressurePlateHandler.cs
+++ Gooseberries/Assets/Scripts/Level/Interactables/PressurePlateHandler.cs
@@ -8,19 +8,29 @@ namespace Interactables
     {
         [HideInInspector] public bool isActive = false;
         [HideInInspector] public ElevatorHandler elevatorHandler;
+
+        bool isPlayerWithinCollider = false;
+        bool isKnightWithinCollider = false;
         
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (collision.tag == "Player") isPlayerWithinCollider = true;
+            if (collision.tag == "Knight") isKnightWithinCollider = true;
             isActive = true;
             elevatorHandler.ResetLerpTime();
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
-            isActive = false;
-            elevatorHandler.ResetLerpTime();
-            elevatorHandler.Disengage();
+            if (collision.tag == "Player") isPlayerWithinCollider = false;
+            if (collision.tag == "Knight") isKnightWithinCollider = false;
+            if (!isPlayerWithinCollider && !isKnightWithinCollider)
+            {
+                isActive = false;
+                elevatorHandler.ResetLerpTime();
+                elevatorHandler.Disengage();
+            }
         }
     }
 }
